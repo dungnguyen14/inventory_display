ede466496a0682cf145c8734b15179cbc512efaa
Nguyen <phanthuydung.nguyen@the-klu.org>
2022-08-03 07:40:11 +0200

    pandas

--- settings.py
+++ settings.py
@@ -138,21 +138,3 @@ INSTALLED_APPS = ['otree']
 AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
 
-import boto3
-
-region_name = 'us-east-1'
-endpoint_url = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
-
-# Uncomment this line to use in production
-# endpoint_url = 'https://mturk-requester.us-east-1.amazonaws.com'
-
-client = boto3.client(
-    'mturk',
-    endpoint_url=endpoint_url,
-    region_name=region_name,
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-)
-
-# This will return $10,000.00 in the MTurk Developer Sandbox
-print(client.get_account_balance()['AvailableBalance'])
